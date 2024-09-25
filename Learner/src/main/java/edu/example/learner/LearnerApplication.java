97c2f5a477470ca1110bfafb358654e2424f9928
Si-rauis <kkyd7189@gmail.com>
2024-09-25 16:37:22 +0900

    Update 기초 의존성 추가 & 데이터베이스 설정

---
+++ Learner/src/main/java/edu/example/learner/LearnerApplication.java
@@ -0,0 +1,13 @@
+package edu.example.learner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnerApplication.class, args);
+    }
+
+}
---
+++ Learner/src/test/java/edu/example/learner/LearnerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.example.learner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
