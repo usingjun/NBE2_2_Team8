8741c93a6deca3d4a6e8f01a807984510aad63a0
남현호 <143480257+namhyeonh0@users.noreply.github.com>
2024-09-26 11:27:04 +0900

    Feature Inquiry 엔티티 생성

---
+++ Learner/src/main/java/edu/example/learner/entity/Inquiry.java
@@ -0,0 +1,48 @@
+package edu.example.learner.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "inquiry")
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+public class Inquiry {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long inquiryId;
+
+    private String inquiryTitle;
+
+    private String inquiryContent;
+
+    @CreatedDate
+    private LocalDateTime inquiryCreateDate;
+
+//    @Enumerated(EnumType.STRING)
+    private String inquiryStatus;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    public void changeInquiryTitle(String inquiryTitle) {
+        this.inquiryTitle = inquiryTitle;
+    }
+
+    public void changeInquiryContent(String inquiryContent) {
+        this.inquiryContent = inquiryContent;
+    }
+
+    public void changeInquiryStatus(InquiryStatus inquiryStatus) {
+        this.inquiryStatus = inquiryStatus.name();
+    }
+}
