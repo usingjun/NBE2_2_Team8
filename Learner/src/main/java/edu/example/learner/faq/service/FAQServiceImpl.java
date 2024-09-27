a4ef76b12e598f4ab9260f465692eaa56d96ee7b
남현호 <143480257+namhyeonh0@users.noreply.github.com>
2024-09-27 11:26:49 +0900

    Update InquiryServiceImpl, AnswerServiceImpl, FAQServiceImpl 어노테이션 추가

--- Learner/src/main/java/edu/example/learner/faq/service/FAQServiceImpl.java
+++ Learner/src/main/java/edu/example/learner/faq/service/FAQServiceImpl.java
@@ -7,12 +7,14 @@ import edu.example.learner.inquiry.repository.InquiryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 @Log4j2
 public class FAQServiceImpl implements FAQService {
     private final FAQRepository faqRepository;
