159604358c0a06d7768b755c50dece2e0a0aeb47
남현호 <143480257+namhyeonh0@users.noreply.github.com>
2024-09-27 12:21:54 +0900

    Update MemberRepository 삭제

--- Learner/src/main/java/edu/example/learner/repository/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.example.learner.repository;
-
-import edu.example.learner.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-
-}
