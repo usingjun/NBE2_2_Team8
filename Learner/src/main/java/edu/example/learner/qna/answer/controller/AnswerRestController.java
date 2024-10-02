a9ae6fe4fa2636e10d1f4d710549aad2dfbf12d7
usingjun <97494494+usingjun@users.noreply.github.com>
2024-10-02 17:43:34 +0900

    Update API 수정

--- Learner/src/main/java/edu/example/learner/qna/answer/controller/AnswerRestController.java
+++ Learner/src/main/java/edu/example/learner/qna/answer/controller/AnswerRestController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/answers")
+@RequestMapping("/answers")
 @RequiredArgsConstructor
 public class AnswerRestController {
     private final AnswerService answerService;
