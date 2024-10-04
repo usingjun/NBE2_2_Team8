2724ecd8962708a40c57370613cb3a456c3374a4
남현호 <143480257+namhyeonh0@users.noreply.github.com>
2024-10-04 14:01:32 +0900

    Update StudyTableRepository, StudyTableController 수정

--- Learner/src/main/java/edu/example/learner/studytable/repository/StudyTableRepository.java
+++ Learner/src/main/java/edu/example/learner/studytable/repository/StudyTableRepository.java
@@ -11,30 +11,30 @@ import java.util.List;
 public interface StudyTableRepository extends JpaRepository<StudyTable, Long> {
 
     //주간 학습 시간
-    @Query("select sum(st.studyTime) from StudyTable st where st.member.memberId = :memberId and st.studyDate between :startDate and :endDate")
+    @Query("select COALESCE(SUM(st.studyTime), 0) from StudyTable st where st.member.memberId = :memberId and st.studyDate between :startDate and :endDate")
     int getWeeklyStudyTime(@Param("memberId") Long memberId, @Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
 
     //연간 학습 시간
-    @Query("select sum(st.studyTime) from StudyTable st where st.member.memberId =:memberId and year(st.studyDate) = :year")
+    @Query("select coalesce(sum(st.studyTime), 0) from StudyTable st where st.member.memberId =:memberId and year(st.studyDate) = :year")
     int getYearlyStudyTime(@Param("memberId") Long memberID, @Param("year") int year);
 
     //주간 완료한 수업
-    @Query("select sum(st.completed) from StudyTable st where st.member.memberId = :memberId and st.studyDate between :startDate and :endDate")
+    @Query("select coalesce(sum(st.completed), 0) from StudyTable st where st.member.memberId = :memberId and st.studyDate between :startDate and :endDate")
     int getWeeklyCompleted(@Param("memberId") Long memberID, @Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
 
     //월간 완료한 수업
-    @Query("select sum(st.completed) from StudyTable st where st.member.memberId = :memberId and month(st.studyDate) = :month")
+    @Query("select coalesce(sum(st.completed), 0) from StudyTable st where st.member.memberId = :memberId and month(st.studyDate) = :month")
     int getMonthlyCompleted(@Param("memberId") Long memberID, @Param("month") int month);
 
     //연간 완료한 수업
-    @Query("select sum(st.completed) from StudyTable st where st.member.memberId =:memberId and year(st.studyDate) = :year")
+    @Query("select coalesce(sum(st.completed), 0) from StudyTable st where st.member.memberId =:memberId and year(st.studyDate) = :year")
     int getYearlyCompleted(@Param("memberId") Long memberID, @Param("year") int year);
 
-    //주간 요약
+    //주간 전체 요약
     @Query("select st.studyDate, st.completed from StudyTable st where st.member.memberId = :memberId and st.studyDate between :startDate and :endDate order by st.studyDate")
     List<Object[]> getWeeklySummary(@Param("memberId") Long memberId, @Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
 
-    //연간 요약
+    //연간 전체 요약
     @Query("select month(st.studyDate), week(st.studyDate), sum(st.completed) from StudyTable st where st.member.memberId = :memberId and year(st.studyDate) = :year group by month(st.studyDate), week(st.studyDate) order by month(st.studyDate), week(st.studyDate)")
     List<Object[]> getYearlySummary(@Param("memberId") Long memberId, @Param("year") int year);
 }
