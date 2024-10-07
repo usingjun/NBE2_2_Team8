450195c4ba08d8689bf381941062bf451a20a1dd
Si-rauis <kkyd7189@gmail.com>
2024-10-07 12:42:24 +0900

    Feature 내 수강 정보보기 완성

--- Learner/src/main/java/edu/example/learner/courseabout/course/repository/search/MemberCourseSearch.java
+++ Learner/src/main/java/edu/example/learner/courseabout/course/repository/search/MemberCourseSearch.java
@@ -1,4 +1,9 @@
 package edu.example.learner.courseabout.course.repository.search;
 
+import edu.example.learner.courseabout.course.entity.MemberCourse;
+
+import java.util.List;
+
 public interface MemberCourseSearch {
+    List<MemberCourse> getMemberCourse(Long memberId);
 }
